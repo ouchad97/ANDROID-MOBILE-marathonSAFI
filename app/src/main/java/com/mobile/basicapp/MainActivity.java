0b707509c48f6373129d7e16be415c6899379b25
ouchad97 <ouchad97@gmail.com>
2021-05-19 22:47:36 +0100

    Home page with slide

---
+++ app/src/main/java/com/mobile/basicapp/MainActivity.java
@@ -0,0 +1,67 @@
+package com.mobile.basicapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.text.Html;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager mSlideViewPager;
+    private LinearLayout mDotLayout;
+
+    private TextView[] mDots;
+
+    private SliderAdapter sliderAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mSlideViewPager = (ViewPager) findViewById(R.id.slideViewPager);
+        mDotLayout = (LinearLayout) findViewById(R.id.dotsLayout);
+
+        sliderAdapter = new SliderAdapter(this);
+        mSlideViewPager.setAdapter(sliderAdapter);
+        addDotsIndicator(0);
+
+        mSlideViewPager.addOnPageChangeListener(viewListener);
+
+    }
+
+    public void addDotsIndicator( int position){
+        mDots = new TextView[3];
+        mDotLayout.removeAllViews();
+        for(int i = 0; i<mDots.length; i++){
+            mDots[i]= new TextView(this);
+            mDots[i].setText(Html.fromHtml("&#8226;"));
+            mDots[i].setTextSize(35);
+            mDots[i].setTextColor(getResources().getColor(R.color.colorTrasparentWhite));
+
+            mDotLayout.addView(mDots[i]);
+        }
+
+        if(mDots.length>0){
+            mDots[position].setTextColor(getResources().getColor(R.color.mycolor));
+        }
+    }
+    ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int i) {
+            addDotsIndicator(i);
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+}
---
+++ app/src/main/java/com/mobile/basicapp/SliderAdapter.java
@@ -0,0 +1,74 @@
+package com.mobile.basicapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+
+public class SliderAdapter extends PagerAdapter {
+
+    Context context;
+    LayoutInflater layoutInflater;
+    public SliderAdapter(Context context){
+        this.context=context;
+    }
+
+    public  int[] slide_image={
+            R.drawable.borjj,
+            R.drawable.ksars,
+            R.drawable.qwas
+    };
+
+    public String[] slide_headings = {
+            "SAFI",
+            "ASFI",
+            "ZAFFIM"
+    };
+
+
+    public String[] slider_desc = {
+            "Lorem ipsum, hekkkkk",
+            "Lorem ipsum, rrrrrrr",
+            "Lorem ipsum, ppppppp"
+    };
+
+    @Override
+    public int getCount() {
+        return slide_headings.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view==(RelativeLayout) object;
+    }
+
+    @Override
+    public Object instantiateItem( ViewGroup container, int position) {
+        layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.slide_layout, container, false);
+
+        ImageView slideImageView = (ImageView) view.findViewById(R.id.slide_image);
+        TextView slideHeading = (TextView) view.findViewById(R.id.slide_heading);
+        TextView slideDescription = (TextView) view.findViewById(R.id.slide_desc);
+
+        slideImageView.setImageResource(slide_image[position]);
+        slideHeading.setText(slide_headings[position]);
+        slideDescription.setText(slider_desc[position]);
+        container.addView(view);
+
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+
+       container.removeView((RelativeLayout)object);
+     }
+}
